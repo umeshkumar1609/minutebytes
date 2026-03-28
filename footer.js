2a8defa1c2a1cbcd8409c60c9d9f34d1b8a630c0
umeshkumar1609 <dhtanaumesh99@gmail.com>
2026-03-28 15:06:42 +0530

    up

--- footer.js
+++ footer.js
@@ -25,6 +25,15 @@ const footerHTML = `
         </div>
     </div>
 </footer>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6LJGFWQLS5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6LJGFWQLS5');
+</script>
 `;
 
 document.addEventListener('DOMContentLoaded', () => {
